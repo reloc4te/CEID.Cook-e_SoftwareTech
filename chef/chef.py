0382d017c6292561b10f2001026412389fa3f71d
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2021-06-06 02:15:28 +0300

    added window popup

--- chef/chef.py
+++ chef/chef.py
@@ -1,1 +1,13 @@
-#Test for chef
+#Initial code 
+
+
+from tkinter import * #Το χρησιμοποιούμε για να εισάγουμε την βιβλιοθήκη για το GUI
+
+root = Tk() 
+
+#Creating a Label Widget
+myLabel = Label(root, text="Hello World")
+#Pack it onto the screen
+myLabel.pack()
+
+root.mainloop() 

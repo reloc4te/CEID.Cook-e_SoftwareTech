952895b9b8b1d55e0cf5e664c1368587a85fa099
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2021-06-06 03:49:54 +0300

    added th menu

--- chef/chef.py
+++ chef/chef.py
@@ -2,12 +2,27 @@
 
 
 from tkinter import * #Το χρησιμοποιούμε για να εισάγουμε την βιβλιοθήκη για το GUI
+from tkinter import ttk
+from tkinter import filedialog
 
-root = Tk() 
+root = Tk() #To κύριο παράθυρό μας
+root.title("Chef's Interface") #Ο τίτλος του παραθύρου
+root.geometry("500x500")
 
-#Creating a Label Widget
-myLabel = Label(root, text="Hello World")
-#Pack it onto the screen
-myLabel.pack()
+my_notebook = ttk.Notebook(root)
+my_notebook.pack(pady=20)
+
+my_frame1 = Frame(my_notebook, width=400, height=500, bg="white")
+my_frame2 = Frame(my_notebook, width=400, height=500, bg="white")
+my_frame3 = Frame(my_notebook, width=400, height=500, bg="white")
+
+my_frame1.grid(row=250, column=498)
+my_frame2.grid(row=250, column=499)
+my_frame3.grid(row=250, column=500)
+
+my_notebook.add(my_frame1, text="Home")
+my_notebook.add(my_frame2, text="My Recipes")
+my_notebook.add(my_frame3, text="Settings")
+
 
 root.mainloop() 
